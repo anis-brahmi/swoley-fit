5df86f80f80a08dd5bc7865289fb5530fe2d308e
anis-brahmi <anisbrahmi840@gmail.com>
2024-11-25 12:10:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+      <>
+          <div className="flex items-center justify-center h-screen bg-blue-500">
+              <h1 className="text-4xl font-bold text-white text-green-300">Hello, Tailwind CSS!</h1>
+          </div>
+      </>
+  )
+}
+
+export default App
